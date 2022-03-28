5da9b9d6fab2a3dfdd4ac13d3f1cadf8deb42f3a
Naiara Rodrigues Guaraldo <nai.rodriguesg@hotmail.com>
2022-03-28 18:42:57 -0300

    exercícios de continuidade

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametro></ComParametro>
+    </div>,
+    document.getElementById('root'))
